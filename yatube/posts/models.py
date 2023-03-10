82a457c4205a8154ff4ee65b5e815cd943b25355
Стас Басов <stas.basov@whatever.com>
2023-03-11 01:51:27 +0300

    Add new project

---
+++ yatube/posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models 
+from django.contrib.auth import get_user_model
+
+User = get_user_model() 
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    ) 
+    group = models.ForeignKey(
+        'Group',
+        verbose_name='Группа',
+        help_text='Группа, к которой будет относиться пост',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts'
+    )
+        
+                             
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
---
+++ yatube/posts/views.py
@@ -0,0 +1,36 @@
+# posts/views.py
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
+
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+# Страница со списком постов
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+    title = 'Записи сообщества'
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
+ 
