167fadc5f04202d4137c155e4c253a1ae8342d1f
Стас Басов <stas.basov@whatever.com>
2023-03-11 01:59:42 +0300

    Редактирование

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-# Из модуля models импортируем модель Post
 from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
     list_display = (
         'pk',
         'text',
@@ -12,17 +10,11 @@ class PostAdmin(admin.ModelAdmin):
         'author',
         'group',
     ) 
-    # Это позволит изменять поле group в любом посте без лишних движений мышкой, прямо из списка постов.
     list_editable = ('group',)
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',) 
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',) 
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
     empty_value_display = '-пусто-' 
     
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
-admin.site.register(Post, PostAdmin)
-# Регистрации модели Group 
+
+admin.site.register(Post, PostAdmin) 
 admin.site.register(Group)
