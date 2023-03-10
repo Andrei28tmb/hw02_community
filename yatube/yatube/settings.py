74d9287bba8434fb5eaba0b823679ac879855f85
Стас Басов <stas.basov@whatever.com>
2023-03-11 02:27:39 +0300

    Редактирование

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} 
+}
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,25 +1,22 @@
-"""yatube URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""Конфигурация URL-адреса yatube
+Список urlpatterns направляет URL-адреса к представлениям. Для получения дополнительной информации, пожалуйста, смотрите:
+ https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Примеры:
+Представления функций
+ 1. Добавьте импорт: из представлений импорта my_app
+ 2. Добавьте URL-адрес в urlpatterns: path(", views.home, name='home')
+Представления на основе классов
+ 1. Добавьте импорт: из other_app.views импортируйте домой
+ 2. Добавьте URL-адрес в urlpatterns: path(", Home.as_view(), name='home')
+Включая другой URLconf
+ 1. Импортируйте функцию include(): из django.urls импортируйте include, путь
+ 2. Добавьте URL-адрес в urlpatterns: path('blog/', include('blog.urls'))
 """
 
-# yatube/urls.py
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-        # импорт правил из приложения posts
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
-] 
+]
