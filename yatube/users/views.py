bec037c3a0511d759773e962bbb43a2315a96ca9
Стас Басов <stas.basov@whatever.com>
2023-04-20 00:44:21 +0300

    спринт 4/2/5

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CreationForm
 
-# Create your views here.
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
