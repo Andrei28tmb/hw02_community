b115be4a4bf0bcd2e635d5546e856b321125c82e
Стас Басов <stas.basov@whatever.com>
2023-04-29 00:57:51 +0300

    Add new file

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group, User
 
@@ -6,11 +7,14 @@ POSTS = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:POSTS]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
